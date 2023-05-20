149d12fe8bb475d6092ba48a111cb3a6bb79edd9
KenyZz <daniel.sampayo.solis@gmail.com>
2023-05-19 18:18:57 -0600

    Add props and dinamic to dos

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import { ToDoFilter } from './ToDoFilter';
 import { CreateToDoButton } from './CreateToDoButton';
 import { ToDoList } from './ToDolist';
 import { TodoItem } from './ToDoItem';
+import React from 'react';
 import './App.css';
+
+const defaultToDo = [
+  { text: "Jugar Onward", completed: false},
+  { text: "Comer", completed: true },
+  { text: "Ir al centro", completed: false },
+  { text: "beber 2 lt de agua", completed: true },
+  { text: "Curso typescript", completed: false },
+  ];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
         
-        <ToDoCounter />
+        <ToDoCounter completed={8} total={10}/>
         <ToDoFilter />
 
         <ToDoList>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
+          {defaultToDo.map(todo => (
+            <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}/>
+          ))}
         </ToDoList>
 
         <CreateToDoButton />
 
       </header>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,8 +1,8 @@
-function TodoItem() {
+function TodoItem(props) {
     return (
         <li>
             <span>V</span>
-            <p>Cambiar pilas a control VR</p>
+            <p>{props.text}</p>
             <span>X</span>
         </li>
     )
